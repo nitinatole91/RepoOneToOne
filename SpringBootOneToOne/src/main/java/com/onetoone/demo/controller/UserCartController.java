a0e79a7881ac71ece7e7e831a0ec309a06807a94
nitinatole91 <nitinatole91@gmail.com>
2024-02-09 22:39:45 +0530

    add new file

--- SpringBootOneToOne/src/main/java/com/onetoone/demo/controller/UserCartController.java
+++ SpringBootOneToOne/src/main/java/com/onetoone/demo/controller/UserCartController.java
@@ -1,6 +1,7 @@
 package com.onetoone.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,9 @@ public class UserCartController {
 	public User postSaveUser(@RequestBody User user) {
 		User saveUser = userService.saveUser(user);
 		return saveUser;
+	}
+	@GetMapping("/getData")
+	public String  getData() {
+		return "hello";
 	}
 }
